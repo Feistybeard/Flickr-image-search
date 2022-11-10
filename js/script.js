3675eed05d15f29f9a7c7010872a6910ff021cea
Marvan Al Kufai <alkufai@gmail.com>
2022-11-10 16:06:08 +0100

    📦 NEW: Add gallery for search reult 👌 IMPROVE: Layout/Css 🐛 FIX: API call returns farm0

--- js/script.js
+++ js/script.js
@@ -1,43 +1,66 @@
-const API_KEY = ""
-const FETCH_URL = `https://api.flickr.com/services/rest?method=flickr.photos.search&api_key=${API_KEY}&format=json&sort=date-taken-asc&per_page=20&nojsoncallback=1`
-const imageListEl = document.querySelector('.images-list')
-const searchInputEl = document.querySelector('#search')
-const searchBtnEl = document.querySelector('#search-btn')
+const API_KEY = "";
+const FETCH_URL = `https://api.flickr.com/services/rest?method=flickr.photos.search&api_key=${API_KEY}&format=json&sort=date-taken-asc&nojsoncallback=1`;
+const imageListEl = document.querySelector(".images-list");
+const searchInputEl = document.querySelector("#search");
+const searchBtnEl = document.querySelector("#search-btn");
+const overlay = document.querySelector(".overlay");
+const overlayImage = overlay.querySelector("img");
 
 async function fetchFromFlickr(query) {
-    try {
-        const response = await fetch(`${FETCH_URL}&text=${query}`)
-        if (response.ok) {
-            const data = await response.json()
-            return data.photos.photo;
-        } else {
-            console.log('Error on response');
-        }
-    } catch (e) {
-        console.log(e);
+  try {
+    const response = await fetch(`${FETCH_URL}&text=${query}`);
+    if (response.ok) {
+      const data = await response.json();
+      return data.photos;
+    } else {
+      console.log("Error on response");
     }
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 async function renderImages(imagesArr) {
-    imagesArr.forEach(element => {
-        imageListEl.innerHTML += `<li><img src='${element}'></li>`
-    })
-} 
-
-function buildImagesUrl(data) {
-    const images = []
-    data.forEach(element => {
-        const imageUrl = `https://farm${element.farm}.staticflickr.com/${element.server}/${element.id}_${element.secret}.jpg`
-        images.push(imageUrl)
-    });
-
-    renderImages(images)
+  imagesArr.forEach((element) => {
+    imageListEl.innerHTML += 
+      `<li class='item'>
+        <img src='${element}' class='img'>
+        <div class="item-overlay">
+          <button>View →</button>
+        </div>
+      </li>`;
+  });
+  const images = document.querySelectorAll(".item");
+  images.forEach((image) => image.addEventListener("click", imgClicked));
+  overlay.addEventListener("click", close);
 }
 
-searchBtnEl.addEventListener('click', e => {
-    const searchQuery = searchInputEl.value
-    imageListEl.innerHTML = ''
-    fetchFromFlickr(searchQuery).then(data => {
-        buildImagesUrl(data)
-    })
-})
+function buildImagesUrl(data) {
+  const images = [];
+  data.photo.forEach((element) => {
+    if (element.farm !== 0) {
+      const imageUrl = `https://farm${element.farm}.staticflickr.com/${element.server}/${element.id}_${element.secret}.jpg`;
+      images.push(imageUrl);
+    }
+  });
+
+  renderImages(images);
+}
+
+function imgClicked(e) {
+  const src = e.currentTarget.querySelector("img").src;
+  overlayImage.src = src;
+  overlay.classList.add("open");
+}
+
+function close() {
+  overlay.classList.remove("open");
+}
+
+searchBtnEl.addEventListener("click", (e) => {
+  const searchQuery = searchInputEl.value;
+  imageListEl.innerHTML = "";
+  fetchFromFlickr(searchQuery).then((data) => {
+    buildImagesUrl(data);
+  });
+});
